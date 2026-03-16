f4865ed3371283604c8ed685bf35c70274db735b
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-03-16 11:56:14 +0100

    Create pjsk.c

---
+++ pjsk.c
@@ -0,0 +1,56 @@
+#include "allshit.h"
+
+int row_p;
+int col_p;
+char phigros_board[30][50];
+
+void initboardphigros(){
+    
+}
+
+///Fonction maîtresse
+void pjsk(){
+    printf("Nuh-uh");
+    fflush(stdout);
+}
+
+void menu_pjsk(){
+    clear();
+    printf("Yo \n"); fflush(stdout);
+    int n = getchar() -48;
+    switch(n){
+        case 1: pjsk(); break;
+        case 2: printf("Buh-bye loser"); break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
