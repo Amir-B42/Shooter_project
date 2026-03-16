f99a051e3eaa96972ac17bb795b41c304797a542
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-03-16 11:55:33 +0100

    Create Jumpingknight.c

---
+++ Jumpingknight.c
@@ -0,0 +1,31 @@
+#include "allshit.h"
+
+
+///Fonction maîtresse
+void jumpingknight(){
+    printf("Actually that's a troll LOL² \nPress something pour revenir au menu");
+    fflush(stdout);
+    int n = 0;
+    while(n != -38){
+      n = getch() - 48;
+    }
+    menu();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
